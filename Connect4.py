bc2257819bcf3948230b1e944bb6906e1068dd3c
Dakotasmith23 <79727934+Dakotasmith23@users.noreply.github.com>
2022-06-27 15:38:10 -0400

    upload

---
+++ Connect4.py
@@ -0,0 +1,148 @@
+import numpy
+import pygame
+import sys
+import math
+
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+
+NUM_ROWS= 6
+NUM_COLUMNS = 7
+
+def createBoard():
+	board = numpy.zeros((NUM_ROWS,NUM_COLUMNS))
+	return board
+
+def dropPiece(board, row, col, piece):
+	board[row][col] = piece
+
+def isValidLocation(board, col):
+	return board[NUM_ROWS-1][col] == 0
+
+def getNextOpenRow(board, col):
+	for r in range(NUM_ROWS):
+		if board[r][col] == 0:
+			return r
+
+def printBoard(board):
+	print(numpy.flip(board, 0))
+
+def winningMove(board, piece):
+	# Check horizontal locations for win
+	for c in range(NUM_COLUMNS-3):
+		for r in range(NUM_ROWS):
+			if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+				return True
+
+	# Check vertical locations for win
+	for c in range(NUM_COLUMNS):
+		for r in range(NUM_ROWS-3):
+			if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+				return True
+
+	# Check positively sloped diaganols
+	for c in range(NUM_COLUMNS-3):
+		for r in range(NUM_ROWS-3):
+			if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+				return True
+
+	# Check negatively sloped diaganols
+	for c in range(NUM_COLUMNS-3):
+		for r in range(3, NUM_ROWS):
+			if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+				return True
+
+def drawBoard(board):
+	for c in range(NUM_COLUMNS):
+		for r in range(NUM_ROWS):
+			pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+			pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+	
+	for c in range(NUM_COLUMNS):
+		for r in range(NUM_ROWS):		
+			if board[r][c] == 1:
+				pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+			elif board[r][c] == 2: 
+				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+	pygame.display.update()
+
+
+board = createBoard()
+printBoard(board)
+gameOver = False
+turn = 0
+
+pygame.init()
+
+SQUARESIZE = 100
+
+width = NUM_COLUMNS * SQUARESIZE
+height = (NUM_ROWS+1) * SQUARESIZE
+
+size = (width, height)
+
+RADIUS = int(SQUARESIZE/2 - 5)
+
+screen = pygame.display.set_mode(size)
+drawBoard(board)
+pygame.display.update()
+
+myfont = pygame.font.SysFont("monospace", 75)
+
+while not gameOver:
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+
+		if event.type == pygame.MOUSEMOTION:
+			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+			posx = event.pos[0]
+			if turn == 0:
+				pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+			else: 
+				pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+		pygame.display.update()
+
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+			#print(event.pos)
+			# Ask for Player 1 Input
+			if turn == 0:
+				posx = event.pos[0]
+				col = int(math.floor(posx/SQUARESIZE))
+
+				if isValidLocation(board, col):
+					row = getNextOpenRow(board, col)
+					dropPiece(board, row, col, 1)
+
+					if winningMove(board, 1):
+						label = myfont.render("Player 1 wins!!", 1, RED)
+						screen.blit(label, (40,10))
+						gameOver = True
+
+
+			# # Ask for Player 2 Input
+			else:				
+				posx = event.pos[0]
+				col = int(math.floor(posx/SQUARESIZE))
+
+				if isValidLocation(board, col):
+					row = getNextOpenRow(board, col)
+					dropPiece(board, row, col, 2)
+
+					if winningMove(board, 2):
+						label = myfont.render("Player 2 wins!!", 1, YELLOW)
+						screen.blit(label, (40,10))
+						gameOver = True
+
+			printBoard(board)
+			drawBoard(board)
+
+			turn += 1
+			turn = turn % 2
+
+			if gameOver:
+				pygame.time.wait(3000)
